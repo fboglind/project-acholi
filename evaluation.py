2bbe290df1d8449b994b1fb0b37c6dac53be7b7a
Oskar Ljung <oskarljung93@gmail.com>
2024-10-29 23:11:47 +0100

    lade till instalering av commet i creae_mt_env.sh

--- evaluation.py
+++ evaluation.py
@@ -1,20 +1,27 @@
-from rouge_metric import PyRouge
-import nltk.translate.nist_score as nist
-# import nltk.translate.meteor_score as meteor
+import nltk.translate.meteor_score as meteor
 # import nltk.translate.ribes_score as ribes
-# import nltk.translate.bleu_score as bleu
+import nltk.translate.bleu_score as bleu
+from comet import download_model, load_from_checkpoint
 
 class eval:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, model_path="Unbabel/XCOMET-XL") -> None:
+        
+        self.model_path = download_model(model_path)
+        self.comet_model = load_from_checkpoint(self.model_path)
     
-    def nist(self, hypothesis: list[str], refferences: list[list[str]]):
-        #TODO implementera nist evaluation
-        return nist.sentence_nist(references=refferences, hypothesis=hypothesis)
+    def bleu(self, hypothesis: list[str], refferences: list[list[str]]):
+        
+        return bleu.sentence_bleu(references=refferences, hypothesis=hypothesis)
     
-    def rouge(self):
-        #TODO implementer rouge evaluation
-        pass
+    def meteor (self,  hypothesis: list[str], refferences: list[list[str]]):
+        
+        return meteor.meteor_score(hypothesis=hypothesis, references=refferences)
+    
+    def comet(self, src: str, hyp: str, ref: str):
+        data = [{"src": src, "mt": hyp, "ref": ref}]
+
+        model_output = self.comet_model.predict(samples=data)
+        return model_output.scores()
 
 if __name__ =="__main__":
     pass 
