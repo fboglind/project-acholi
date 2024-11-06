7200eeace3e534eeab272f76025486d03619bb5a
Fredrik Boglind <frbo5627@rackham3.uppmax.uu.se>
2024-11-06 20:16:58 +0100

    added updated evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -8,7 +8,7 @@ import statistics
 #Unbabel/XCOMET-XL
 #Unbabel/wmt22-comet-da
 class eval:
-    def __init__(self, source_file, translation_out, refference_file, model_path="Unbabel/wmt20-comet-qe-da") -> None:
+    def __init__(self, source_file, translation_out, refference_file, model) -> None:
         """this class contains methods to evaluate the quality of a machine translation. to use it, pass three files of paralell translation
         one file conatins the untranslated source text, one is a reliable paralell translation of the source file, 
         and one contains a machine traslated attempt att translating the source file
@@ -19,8 +19,9 @@ class eval:
         Args:
             source_file (str): path to a soruce file from which the translation is done
             translation_out (str): path to a file where a output of a translation model is written
-            refference_file (str): path to a flie where the paralell refference translation of the source file is written
-            model_path (str, optional): the name of hte comet translation model. Defaults to "Unbabel/wmt20-comet-qe-da". others include "Unbabel/wmt22-comet-da" and "Unbabel/XCOMET-XL" though the latter does not seem to work. check https://huggingface.co/Unbabel for others.
+            refference_file (str): path to a flie where the paralell refference translation of the source
+            model (str, optional): a comet model aquired with comet.download_model(["model name"]). Normally "Unbabel/wmt20-comet-qe-da". 
+            others include "Unbabel/wmt22-comet-da" https://huggingface.co/Unbabel for more.
         """
         
         self.bleu_score = float
@@ -32,7 +33,7 @@ class eval:
         self.src = source_file
         self.trans = translation_out
         self.ref = refference_file
-        self.model_path = download_model(model_path)
+        self.model_path = model
         self.comet_model = load_from_checkpoint(self.model_path)
         
         nltk.download('wordnet')
@@ -65,7 +66,7 @@ class eval:
         """calculates comet score for all lines
 
         Args:
-            data list[dict]): dictionary of source, machine translation and reference (src, mt, ref respecitvely) must be in the followint format:
+            data list[dict]): dictionary of source, machine translation and refference (src, mt, ref respecitvely) must be in the followint format:
             [{
                 "src": "10 到 15 分钟可以送到吗",
                 "mt": "Can I receive my food in 10 to 15 minutes?",
@@ -84,8 +85,8 @@ class eval:
         model_output = self.comet_model.predict(samples=data)
         return model_output.scores
     
-    def full_evaluation(self):
-        """_summary_
+    def full_evaluation(self, do_you_want_to_run_comet=True):
+        """makes a full evaluation of the translation using METEOR, COMET and BLEU
         """
         src = []
         hypothesis = []
@@ -105,27 +106,38 @@ class eval:
         
         for hyp, ref in zip (hypothesis, refference):       
             self.meteor_score_list.append(self.meteor(refferences=[ref], hypothesis=hyp))
-       
-        comet_data = []
-        for src, hyp, ref in zip (src, hypothesis, refference):
-            
-            comet_data.append({"src":src, "mt": " ".join(hyp), "ref": " ".join(ref)})
-          
-        self.comet_score_list = (self.comet(comet_data))
-        self.comet_score = statistics.mean(self.comet_score_list)
+        if do_you_want_to_run_comet:
+            print("running comet")
+            comet_data = []
+            for src, hyp, ref in zip (src, hypothesis, refference):
+                
+                comet_data.append({"src":src, "mt": " ".join(hyp), "ref": " ".join(ref)})
+        
+        if do_you_want_to_run_comet:    
+            self.comet_score_list = (self.comet(comet_data))
+            self.comet_score = statistics.mean(self.comet_score_list)
+        
         self.meteor_score = statistics.mean(self.meteor_score_list)
         self.bleu_score = self.bleu(hypothesis=hypothesis, refferences=bleu_refference)
         
         # printing out the resiults for now. make better output later.       
-        print ("comet score: ", self.comet_score)
+        print ("COMET score: ", self.comet_score)
         print ("METEOR score: ", self.meteor_score)
         print ("BLEU score: ",  self.bleu_score)
+
+
+
+
 if __name__ =="__main__":
 
     # so far, this is the interface. make an argparse later
     source_file = "processed_data_moses/salt.test.tk.lc.ach"
     translation_out = "translations_20241102_175428/trans_step8000_beam5_batch32.txt"
     refference_file  = "processed_data_moses/salt.test.tk.lc.eng"
-    ev = eval(source_file, translation_out, refference_file)
+    
+    comet_model = download_model("Unbabel/wmt20-comet-qe-da")
+    ev = eval(source_file, translation_out, refference_file, comet_model)
+   
     
     ev.full_evaluation()
+   
